4f7ceb5b7698cabf874458f8aff1a07ab8316fe2
Zakhrov <aaron.zakhrov@gmail.com>
2016-02-18 15:40:33 +0530

    Add Authors

--- aboutdialog.cpp
+++ aboutdialog.cpp
@@ -1,6 +1,6 @@
 #include "aboutdialog.h"
 #include "ui_aboutdialog.h"
-
+#include <QMessageBox>
 AboutDialog::AboutDialog(QWidget *parent) :
     QDialog(parent),
     ui(new Ui::AboutDialog)
@@ -11,8 +11,8 @@ AboutDialog::AboutDialog(QWidget *parent) :
     simg=img.scaled(128,128);
     ui->label->setPixmap(simg);
     ui->label_3->setText(QApplication::applicationVersion());
-    ui->label_4->setText(QApplication::organizationName());
-    ui->label_5->setText(QApplication::organizationDomain());
+    //ui->label_4->setText(QApplication::organizationName());
+    //ui->label_5->setText(QApplication::organizationDomain());
 
 }
 
@@ -24,4 +24,11 @@ AboutDialog::~AboutDialog()
 void AboutDialog::on_pushButton_clicked()
 {
     QApplication::aboutQt();
+}
+
+void AboutDialog::on_pushButton_2_clicked()
+{
+    QMessageBox Authors;
+    Authors.setText("Lead Developer: Aaron Dominick \n Developers: Neha A, Aishwarya P");
+    Authors.exec();
 }
