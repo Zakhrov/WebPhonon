d3d6c7d80578f281ddf21c9e4f2b5c7f7ebca5bf
Zakhrov <aaron.zakhrov@gmail.com>
2016-02-19 17:59:41 +0530

    Add github repos to creditsdialog.cpp

--- creditsdialog.cpp
+++ creditsdialog.cpp
@@ -10,8 +10,8 @@ CreditsDialog::CreditsDialog(QWidget *parent) :
     QPixmap LeadImg = QPixmap(":/profiles/AMD_Logo11.jpg");
     ui->label_2->setPixmap(LeadImg.scaled(64,64));
     ui->label_3->setText("Aaron Dominick [https://github.com/Zakhrov]");
-    ui->label_6->setText("K. Aishwarya Pawar []");
-    ui->label_8->setText("Nithya Agarwal []");
+    ui->label_6->setText("K. Aishwarya Pawar [https://github.com/aish942]");
+    ui->label_8->setText("Neha Agarwal [https://github.com/neha947]");
 
 }
 
